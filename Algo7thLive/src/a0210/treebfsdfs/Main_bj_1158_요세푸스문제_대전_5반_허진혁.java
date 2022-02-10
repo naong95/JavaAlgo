a61f7802f6f3296877e561cefc27d8c5a000700f
sonic1 <sonic1@naver.com>
2022-02-10 14:12:31 +0900

    bfs, dfs 추가

---
+++ Algo7thLive/src/a0210/treebfsdfs/Main_bj_1158_요세푸스문제_대전_5반_허진혁.java
@@ -0,0 +1,26 @@
+package a0210.treebfsdfs;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_1158_요세푸스문제_대전_5반_허진혁 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("res/input_bj_1158.txt")));
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+//		1 2 3 4 5 6 7 
+//		1 2 4 5 6 7    3
+//		1 2 4 5 7      6
+//		1 4 5 7        2
+//		1 4 5          7
+//		1 4            5
+//		4              1
+//		               4
+		
+	}
+}
