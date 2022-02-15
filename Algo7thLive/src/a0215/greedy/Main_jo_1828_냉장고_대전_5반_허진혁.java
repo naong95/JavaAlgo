1569cc4f7c676372c967356385c8513e944d0e88
sonic1 <sonic1@naver.com>
2022-02-16 01:21:06 +0900

    냉장고 수정

--- Algo7thLive/src/a0215/greedy/Main_jo_1828_냉장고_대전_5반_허진혁.java
+++ Algo7thLive/src/a0215/greedy/Main_jo_1828_냉장고_대전_5반_허진혁.java
@@ -6,57 +6,40 @@ import java.util.*;
 public class Main_jo_1828_냉장고_대전_5반_허진혁 {
 
 	/**
-	 * 오늘 배웠던 회의실 시작시간 끝시간을 알 때 가장 많은 회의를 잡는 아이디어로 접근하면 겹칠 때 카운트 해서 카운트 최솟값
+	 * 정렬을 이용하자
 	 */
-
-	static class chemical implements Comparable<chemical> {
-		int min, max;
-
-		public chemical(int min, int max) {
-			super();
-			this.min = min;
-			this.max = max;
-		}
-
-		@Override
-		public int compareTo(chemical o) {
-			return this.max != o.max ? this.max - o.max : this.min - o.min;
-		}
-
-	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("res/input_jo_1828.txt")));
 //		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());// 화학물질 개수
-		chemical[] chemicals = new chemical[N];// 화학물질 클래스로 이루어진 N개의 배열
-		for (int i = 0; i < N; i++) {
-			String[] tmp = br.readLine().split(" ");
-			chemicals[i] = new chemical(Integer.parseInt(tmp[0]), Integer.parseInt(tmp[1]));
+		int[][] xy = new int[N][2];// 각 화학물질 최저x 최대y 온도 0, 1에 넣는 배열
+		int cnt = 1;// 냉장고의 개수는 기본 1개
+
+		for (int i = 0; i < N; i++) {// 최저, 최대 온도 대입
+			String[] line = br.readLine().split(" ");
+			xy[i][0] = Integer.parseInt(line[0]);
+			xy[i][1] = Integer.parseInt(line[1]);
 		}
-
-		System.out.println(counter(chemicals));
-
-		br.close();
-	}
-
-	public static int counter(chemical[] chemicals) {
-		int count = 0;
-		Arrays.sort(chemicals);
-		int size = chemicals.length;
-		for (int i = 1; i < size; i++) {
-			if (chemicals[i - 1].max < chemicals[i].min) {
-				count++;
+		Arrays.sort(xy, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return Integer.compare(o1[1], o2[1]);// 최고온도를 오름차순으로 정렬
+			}// 정렬을 하고 나면 다음 원소의 최저온도가 최고 온도보다 크다면 냉장고를 하나 늘리면 된다
+		});
+		int max = xy[0][1];// 가장 최고 온도가 작은 첫번째 값을 최댓값으로 설정해주고
+		for (int i = 0; i < N; i++) {// max는 바로 전 화학물질의 최고 온도가 된다
+			if (xy[i][0] <= max) {// 들어온 화학 물질의 최저온도 보다 max가 크거나 같고
+				if (xy[i][1] < max) {// 최고온도보다도 크다면
+					max = xy[i][1];// 전 화학물질의 최고 온도 갱신
+				}
+				continue;// 만약 최고온도가 더 커서 max가 화학 물질의 중간이라면 continue
+			} else {// 들어왔던 화학 물질들의 최고 온도보다 새로 들어온 화학물질의 최저 온도가 높다면
+				max = xy[i][1];// 최고 온도를 갱신하고 현재 있는 냉장고에는 넣을 수 없으므로
+				cnt++;// 냉장고 1개 추가
 			}
 		}
-		if (count == 0) {
-			if (size % 3 == 0)
-				return size / 3;
-			else if (size % 3 != 0)
-				return size / 3 + 1;
-		}
-
-		return size / 3 + count;
-
+		System.out.println(cnt);
+		br.close();
 	}
 }
