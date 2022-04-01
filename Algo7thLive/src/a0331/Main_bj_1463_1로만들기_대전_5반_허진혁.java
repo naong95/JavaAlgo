0e0a08fbf543a89924148d60451ed0ee3579938a
sonic1 <sonic1@naver.com>
2022-04-01 13:07:27 +0900

    1로 만들기

--- Algo7thLive/src/a0331/Main_bj_1463_1로만들기_대전_5반_허진혁.java
+++ Algo7thLive/src/a0331/Main_bj_1463_1로만들기_대전_5반_허진혁.java
@@ -12,13 +12,13 @@ public class Main_bj_1463_1로만들기_대전_5반_허진혁 {
 		int N = Integer.parseInt(br.readLine());
 		int[] dp = new int[N + 1];
 		
-		dp[0] = 0;
-		for(int i = 1; i <= N; i++) {
+		dp[1] = 0;
+		for(int i = 2; i <= N; i++) {
 			dp[i] = dp[i - 1] + 1;
-			if(i % 3 == 0) dp[i] = Math.min(dp[i - 1], dp[i / 3] + 1);
-			if(i % 2 == 0) dp[i] = Math.min(dp[i - 1], dp[i / 2] + 1);
+			if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
 		}
-		System.out.println(Arrays.toString(dp));
+		System.out.println(dp[N]);
 		
 		br.close();
 	}
