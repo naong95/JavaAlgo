a1258a40a0fd1b9a5de86c1a55313d7db2e00ea6
sonic1 <sonic1@naver.com>
2022-04-01 11:18:40 +0900

    갱신

---
+++ Algo7thLive/src/a0331/Main_bj_1463_1로만들기_대전_5반_허진혁.java
@@ -0,0 +1,25 @@
+package a0331;
+
+import java.util.*;
+import java.io.*;
+/*
+
+*/
+public class Main_bj_1463_1로만들기_대전_5반_허진혁 {
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/input_bj_1463.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
+		
+		dp[0] = 0;
+		for(int i = 1; i <= N; i++) {
+			dp[i] = dp[i - 1] + 1;
+			if(i % 3 == 0) dp[i] = Math.min(dp[i - 1], dp[i / 3] + 1);
+			if(i % 2 == 0) dp[i] = Math.min(dp[i - 1], dp[i / 2] + 1);
+		}
+		System.out.println(Arrays.toString(dp));
+		
+		br.close();
+	}
+}
---
+++ Algo7thLive/src/study/Main_bj_15903_카드합체놀이_대전_5반_허진혁.java
@@ -0,0 +1,23 @@
+package study;
+
+import java.util.*;
+import java.io.*;
+/*
+석환이는 응애다.
+
+1. x번 카드와 y번 카드를 골라 그 두 장에 쓰여진 수를 더한 값을 계산한다. (x ≠ y)
+2. 계산한 값을 x번 카드와 y번 카드 두 장 모두에 덮어 쓴다.
+
+이 카드 합체를 총 m번 하면 놀이가 끝난다. m번의 합체를 모두 끝낸 뒤,
+n장의 카드에 쓰여있는 수를 모두 더한 값이 이 놀이의 점수가 된다.
+이 점수를 가장 작게 만드는 것이 놀이의 목표이다.
+*/
+public class Main_bj_15903_카드합체놀이_대전_5반_허진혁 {
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/input_bj_15903.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		br.close();
+	}
+}
