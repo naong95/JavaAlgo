619991e4640155875d7318a46780b0398bd4c9f4
sonic1 <sonic1@naver.com>
2022-08-24 23:53:47 +0900

    bj 1003 success

--- Algo7thLive/src/무지성백준/Main_bj_1003_피보나치함수.java
+++ Algo7thLive/src/무지성백준/Main_bj_1003_피보나치함수.java
@@ -2,33 +2,26 @@ package 무지성백준;
 import java.util.*;
 import java.io.*;
 /*
-
+dp는 4,5번째 케이스까지 나열해서 규칙이 있는지부터 파악하자 
 */
 public class Main_bj_1003_피보나치함수 {
-	static int[] zero;
-	static int[] one;
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("muji/input_bj_1003.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for(int i = 1; i <= T; i++) {
 			int N = Integer.parseInt(br.readLine());
-			zero = new int[41];
-			one = new int[41];
-			fibo(N);
+			int zero = 1;
+			int one = 0;
+			int zero_plus_one = 1;
+			 
+			for (int j = 0; j < N; j++) {
+				zero = one;	
+				one = zero_plus_one;	
+				zero_plus_one = zero + one;	
+			}
 			System.out.println(zero + " " + one);
 		}
 		br.close();
-	}
-	static int fibo(int n) {
-		if(n == 0) {
-			zero[n]++;
-			return 0;
-		}
-		else if(n == 1) {
-			one[n]++;
-			return 1;
-		}
-		else return fibo(n - 1) + fibo(n - 2);
 	}
 }
