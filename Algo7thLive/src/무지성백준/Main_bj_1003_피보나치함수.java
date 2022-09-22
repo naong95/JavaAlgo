c71ab6f7cfa159d0a1a7c23285e1013e9ac169ea
sonic1 <sonic1@naver.com>
2022-09-23 00:21:14 +0900

    push test

--- Algo7thLive/src/무지성백준/Main_bj_1003_피보나치함수.java
+++ Algo7thLive/src/무지성백준/Main_bj_1003_피보나치함수.java
@@ -1,6 +1,9 @@
 package 무지성백준;
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 /*
 dp는 4,5번째 케이스까지 나열해서 규칙이 있는지부터 파악하자 
 */
@@ -9,16 +12,16 @@ public class Main_bj_1003_피보나치함수 {
 		System.setIn(new FileInputStream("muji/input_bj_1003.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		for(int i = 1; i <= T; i++) {
+		for (int i = 1; i <= T; i++) {
 			int N = Integer.parseInt(br.readLine());
 			int zero = 1;
 			int one = 0;
 			int zero_plus_one = 1;
-			 
+
 			for (int j = 0; j < N; j++) {
-				zero = one;	
-				one = zero_plus_one;	
-				zero_plus_one = zero + one;	
+				zero = one;
+				one = zero_plus_one;
+				zero_plus_one = zero + one;
 			}
 			System.out.println(zero + " " + one);
 		}
