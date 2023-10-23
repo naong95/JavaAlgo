dbf692ee7f1dbdef0eaeedb4c655e71d8a93778c
jin <jin@DESKTOP-HHK0NGF>
2023-10-23 16:47:41 +0900

    git push test

---
+++ Algo7thLive/Albbano/baekjoon/Main_bj_11382_꼬마정민_대전_5반_허진혁.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+/*
+
+*/
+public class Main_bj_11382_꼬마정민_대전_5반_허진혁 {
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("albbanoRes/input_bj_11382.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		long c = Long.parseLong(st.nextToken());
+		System.out.print(a + b + c);
+		br.close();
+	}
+}
