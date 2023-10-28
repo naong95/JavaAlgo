ef6b336213adadda8716ee29bfa99dde445f49bb
jin <sonic1@naver.com>
2023-10-28 12:17:44 +0900

    231028 algo push

---
+++ Algo7thLive/Albbano/baekjoon/Main_bj_2738_행렬덧셈_대전_5반_허진혁.java
@@ -0,0 +1,42 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+/*
+
+*/
+public class Main_bj_2738_행렬덧셈_대전_5반_허진혁 {
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("albbanoRes/input_bj_2738.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] map = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < M; j++) {
+				map[i][j] += Integer.parseInt(st.nextToken());
+			}
+		}
+		StringBuilder sb = new StringBuilder("");
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < M; j++) {
+				sb.append(map[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString().trim());
+		br.close();
+	}
+}
