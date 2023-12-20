61c6d133424cecae81b9e22e71f347b28fa90dd7
jin <sonic1@naver.com>
2023-12-20 16:10:58 +0900

    231220 algo push

--- Algo7thLive/Albbano/baekjoon/Main_bj_30980_여중생파댕이와공부를_대전_5반_허진혁.java
+++ Algo7thLive/Albbano/baekjoon/Main_bj_30980_여중생파댕이와공부를_대전_5반_허진혁.java
@@ -9,8 +9,23 @@ public class Main_bj_30980_여중생파댕이와공부를_대전_5반_허진혁 
 		System.setIn(new FileInputStream("albbanoRes/input_bj_30980.txt")); 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		
+		char[][] map = new char[3*n][8*m];
 		
+		for(int i = 0; i < 3*n; i++) {
+			map[i] = br.readLine().toCharArray();
+		}
+		Queue<int[]> q = new ArrayDeque<>();
+		for(int i = 0; i < 3*n; i++) {
+			for(int j = 0; j < 8*m; j++) {
+				if(map[i][j] == '+') {
+					q.offer(new int[] {i, j});
+				}
+			}
+		}
 		
 		br.close();
 	}
