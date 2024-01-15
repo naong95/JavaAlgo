8cb766ccf8505f2576cd9d33bce7a37dcc18f1b6
jin <sonic1@naver.com>
2024-01-15 14:54:47 +0900

    240115 algo push

--- Algo7thLive/Albbano/baekjoon/Main_bj_30980_여중생파댕이와공부를_대전_5반_허진혁.java
+++ Algo7thLive/Albbano/baekjoon/Main_bj_30980_여중생파댕이와공부를_대전_5반_허진혁.java
@@ -58,20 +58,12 @@ public class Main_bj_30980_여중생파댕이와공부를_대전_5반_허진혁 
 		for (int i = 0; i < 3 * n; i++) {
 			map[i] = br.readLine().toCharArray();
 		}
-		Queue<int[]> q = new ArrayDeque<>();
 		for (int i = 0; i < 3 * n; i++) {
 			for (int j = 0; j < 8 * m; j++) {
 				if (map[i][j] == '+') {
-					q.offer(new int[] { i, j });
+					study(i, j, map);
 				}
 			}
-		}
-
-		while (!q.isEmpty()) {
-			int i = q.peek()[0];
-			int j = q.poll()[1];
-
-			study(i, j, map);
 		}
 
 		StringBuilder sb = new StringBuilder("");
