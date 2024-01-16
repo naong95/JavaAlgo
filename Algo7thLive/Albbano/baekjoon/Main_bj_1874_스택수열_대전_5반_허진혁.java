66d1d377d7284c031b821994edc09b34ab474df2
jin <sonic1@naver.com>
2024-01-16 14:09:05 +0900

    240116 algo push

--- Algo7thLive/Albbano/baekjoon/Main_bj_1874_스택수열_대전_5반_허진혁.java
+++ Algo7thLive/Albbano/baekjoon/Main_bj_1874_스택수열_대전_5반_허진혁.java
@@ -1,38 +1,44 @@
 package baekjoon;
+
 import java.util.*;
 import java.io.*;
+
 /*
 
 */
 public class Main_bj_1874_스택수열_대전_5반_허진혁 {
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("albbanoRes/input_bj_1874.txt")); 
+		System.setIn(new FileInputStream("albbanoRes/input_bj_1874.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n+1];
 		StringBuilder sb = new StringBuilder("");
-		
+
 		Stack<Integer> s = new Stack<>();
-		for(int i = 1; i < n+1; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		for(int i = 1; i < n+1; i++) {
-			if(!s.isEmpty()) {
-				if(s.peek() == arr[i]) {
-					s.pop();
-					sb.append("-").append("\n");
-				} else {
+		int start = 0;
+
+		while (n-- > 0) {
+			int value = Integer.parseInt(br.readLine());
+
+			if (start < value) {
+
+				for (int i = start + 1; i < value + 1; i++) {
 					s.push(i);
 					sb.append("+").append("\n");
 				}
-			} else {
-				s.push(i);
-				sb.append("+").append("\n");
+				start = value;
 			}
+
+			else if (s.peek() != value) {
+				System.out.println("NO");
+				return;
+			}
+
+			s.pop();
+			sb.append("-").append("\n");
+
 		}
-		System.out.println(s);
+
 		System.out.println(sb.toString());
 		br.close();
 	}
