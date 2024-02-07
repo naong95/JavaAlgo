ae24d271da49aa78e3f255117552f0c337eb2f8f
jin <sonic1@naver.com>
2024-02-07 13:54:34 +0900

    240207 algo push

--- Algo7thLive/Albbano/baekjoon/Main_bj_15650_N과M2_대전_5반_허진혁.java
+++ Algo7thLive/Albbano/baekjoon/Main_bj_15650_N과M2_대전_5반_허진혁.java
@@ -12,6 +12,22 @@ public class Main_bj_15650_N과M2_대전_5반_허진혁 {
 	static int N, M;
 	static StringBuilder sb = new StringBuilder("");
 	
+	public static void dfs(int start, int depth) {
+		if(depth == M) {
+			for(int val : arr) {
+				sb.append(val).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = start; i <= N; i++) {
+			arr[depth] = i;
+			dfs(i + 1, depth + 1);
+		}
+	}
+	
+	
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("albbanoRes/input_bj_15650.txt"));
 		Scanner sc = new Scanner(System.in);
@@ -20,24 +36,9 @@ public class Main_bj_15650_N과M2_대전_5반_허진혁 {
 		M = sc.nextInt();
 		arr = new int[M];
 		
-		dfs(0, 1);
+		dfs(1, 0);
 		
 		System.out.println(sb.toString());
 		sc.close();
-	}
-	
-	public static void dfs(int depth, int start) {
-		if(depth == M) {
-			for(int val : arr) {
-				sb.append(val).append(" ");
-			}
-			sb.append("\n");			
-			return;
-		}
-		
-		for(int i = start; i <= N; i++) {
-			arr[depth] = i;
-			dfs(depth + 1, start + 1);
-		}
 	}
 }
