387b49c312c75f8a73051a4bd6dfe4fbaab19581
jin <sonic1@naver.com>
2024-03-18 23:14:00 +0900

    240318 algo push

---
+++ Algo7thLive/sam1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+
+public class solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.close();
+	}
+}
