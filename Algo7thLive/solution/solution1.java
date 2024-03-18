e7f0da1f63d993bd1adcc1d00ddf4e8b8726ab5d
jin <sonic1@naver.com>
2024-03-18 23:26:57 +0900

    240318 algo push

--- Algo7thLive/solution/solution1.java
+++ Algo7thLive/solution/solution1.java
@@ -1,10 +1,14 @@
 import java.util.*;
 import java.io.*;
+/*
 
-
-public class solution {
+*/
+public class solution1 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("test");
+		
 		br.close();
 	}
 }
