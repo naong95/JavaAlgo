1aa6a5df92d9c4aece1126ad756536e0d9fbf07a
jin <sonic1@naver.com>
2024-04-02 20:47:16 +0900

    240402 algo push

---
+++ Algo7thLive/solution/solution2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+//루돌프의 반란
+public class solution2 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+	}
+}
