accb7dd3e80079467a47e7eed1dd09a4c9a0b68a
jin <sonic1@naver.com>
2024-10-10 14:55:01 +0900

    241011 algo push

--- Algo7thLive/solution/solution2.java
+++ Algo7thLive/solution/solution2.java
@@ -5,7 +5,15 @@ public class solution2 {
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		
+		sc.useDelimiter(".");
 		
+		int y = sc.nextInt();
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.printf("%d-%d-%d", m, d, y);
+        
+        int a = 1;
 		sc.close();
 	}
 }
